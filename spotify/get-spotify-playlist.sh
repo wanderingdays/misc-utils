2a6aa5a8bb50f46e18acc95dde51a960d456a2c6
wanderingdays <reznor@wanderingdays.com>
2019-01-31 15:36:36 -0800

    2nd get playlist

--- spotify/get-spotify-playlist.sh
+++ spotify/get-spotify-playlist.sh
@@ -2,12 +2,18 @@
 
 cred_file="credential.json"
 client_id=$(cat $cred_file |jq -r '.client_id')
-echo $client_id
 client_secret=$(cat $cred_file |jq -r '.client_secret')
-echo $client_secret
 cred="$(echo -n "$client_id:$client_secret" | base64 -w 0)"
-echo $cred
-token=$(curl -s -v -X "POST" -H "Authorization: Basic $cred" -H "Accept: application/json" -d "grant_type=client_credentials" "https://accounts.spotify.com/api/token" 2>/dev/null | jq -r '.access_token')
-echo $token
+token=$(curl -v -s -X "POST" -H "Authorization: Basic $cred" -H "Accept: application/json" -d "grant_type=client_credentials" "https://accounts.spotify.com/api/token" 2>/dev/null | jq -r '.access_token')
 
+fulllist=$(curl -v -H "Authorization: Bearer $token" https://api.spotify.com/v1/playlists/6EcUtQ0fz02ajr0E6ImMjP)
+echo $fulllist | jq .
 
+echo $fulllist | jq -r '.tracks.items[].track.name' | while read track ; do
+#    echo $fulllist | jq -r '.tracks.items[]  | select(.track.name == $track) | .artists[].name'
+    echo $track
+done
+#name=$(echo $fulllist | jq -r '.name')
+#mkdir $name
+#imgurl=$(echo $fulllist | jq -r '.images[0].url')
+#curl -o ./$name/cover.jpg $imgurl
